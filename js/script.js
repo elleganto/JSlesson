7fedf056f4127fff4e79796f232227ec908dfe7a
Nurlan <akunur@mail.ru>
2021-04-08 13:51:14 +0600

    1st practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из проследних простмотренных фильмов", ""),
+  b = prompt("На сколько оцените его", ""),
+  c = prompt("Один из проследних простмотренных фильмов", ""),
+  d = prompt("На сколько оцените его", "");
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
